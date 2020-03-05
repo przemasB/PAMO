773634719728aaba1b95c8f1c8cc8c417f4bd9d5
unknown <przemyslaw.bialczak@pjwstk.edu.pl>
2020-03-05 21:03:33 +0100

    first commit - BMI calculator_v1

---
+++ BMICalculator_1/app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.bmicalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static java.lang.Math.pow;
+
+public class MainActivity extends AppCompatActivity {
+    EditText weight, height;
+    TextView resulttext;
+    String calculation, BMIresult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        weight = findViewById(R.id.weight);
+        height = findViewById(R.id.height);
+        resulttext = findViewById(R.id.result);
+
+    }
+
+    public void calculateBMI(View view) {
+        String S1 = weight.getText().toString();
+        String S2 = height.getText().toString();
+
+        float weightValue = Float.parseFloat(S1);
+        float heightValue = Float.parseFloat(S2);
+
+        float bmi = weightValue / (heightValue/100 * heightValue/100);
+
+        if (bmi < 18.5){
+            BMIresult = "Under Weight";
+        }
+        else if (bmi >= 18.5 && bmi <= 24.9) {
+            BMIresult = "Your Weight is fine";
+        }
+        else if (bmi >=25 && bmi <=29.9){
+            BMIresult = "Over Weight";
+        }
+        else {
+            BMIresult = "U are really FAT";
+        }
+
+        calculation = "Result: \n\n" + bmi + "\n" + BMIresult;
+        resulttext.setText(calculation);
+    }
+
+}
