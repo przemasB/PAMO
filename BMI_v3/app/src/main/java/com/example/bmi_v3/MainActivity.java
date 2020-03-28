df3b39b35e5732e91513397e2ba78b8830b1eb42
unknown <przemyslaw.bialczak@pjwstk.edu.pl>
2020-03-28 15:03:14 +0100

    added graph

--- BMI_v3/app/src/main/java/com/example/bmi_v3/MainActivity.java
+++ BMI_v3/app/src/main/java/com/example/bmi_v3/MainActivity.java
@@ -4,17 +4,34 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    float numberOfDeaths[] = {9134, 4934, 3292, 2378, 1696, 1423, 759, 2791};
+    String countryNames[]={"ITALY", "SPAIN", "CHINA", "IRAN", "FRANCE", "USA", "UK", "OTHER"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupPieChart();
+
 
         //initialize and assign variable
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
@@ -51,4 +68,37 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void setupPieChart() {
+        List<PieEntry> pieEntries = new ArrayList<>();
+        for (int i = 0; i < numberOfDeaths.length; i++){
+            pieEntries.add(new PieEntry(numberOfDeaths[i], countryNames[i]));
+
+        }
+        PieDataSet dataSet = new PieDataSet(pieEntries, "");
+        dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        PieData data = new PieData(dataSet);
+        data.setValueTextSize(15f);
+        data.setValueTextColor(Color.BLACK);
+
+
+        PieChart chart = findViewById(R.id.chart);
+        chart.setData(data);
+        //  chart.animateY(1000);
+        chart.invalidate();
+        chart.setDrawHoleEnabled(false);
+
+        Legend legend = chart.getLegend();
+        legend.setEnabled(false);
+
+
+        Description description = chart.getDescription();
+        description.setEnabled(true);
+        description.setText("CoronaVirus death toll by countries (27/03/20)");
+        description.setTextSize(17f);
+        description.setPosition(1075,100);
+
+
+    }
+
 }
